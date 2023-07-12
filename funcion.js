4029be568a2cc237822e74704a475f27bcde7f3d
jsebastian08 <juansebastian0197@hotmail.com>
2023-07-11 21:36:26 -0500

    Pokedex con tarjeta solo nombre sin imagenes y colores

--- funcion.js
+++ funcion.js
@@ -1,39 +1,26 @@
-const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
-const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+const pokecontainer = document.querySelector("#pokem");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for(let i= 1; i <= 151; i++){
+	fetch(URL + i)
+		.then((response) => response.json())
+		.then(data => mostrarPokemon(data))
 }
+function mostrarPokemon(poke){
+	const div = document.createElement("div");
+	div.classList.add("pokemon");
+	div.innerHTML = `
+	
+	<div class="img-container">
+	  <img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="Bulbasaur">
+	</div>
+	<div class="info">
+	  <span class="number">001</span>
+	  <h3 class="name">${poke.name}</h3>
+	  <small class="type">Type: <span>grass</span></small>
+	</div>`;
 
-const main_types = Object.keys(colors)
-
-const fetchPokemons = async () => {
-    for(let i = 1; i <= pokemon_count; i++) {
-        await getPokemon(i)
-    }
+  pokecontainer.append(div);
+	
+	
 }
-
-const getPokemon = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    const res = await fetch(url)
-    const data = await res.json()
-    createPokemonCard(data)
-}
-
-const createPokemonCard = (pokemon) => {
-    
-}
-
-fetchPokemons()
