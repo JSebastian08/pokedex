391319c7652899f950662429d81c733958dba8fe
jsebastian08 <juansebastian0197@hotmail.com>
2023-07-11 22:35:49 -0500

    Pokedex sin numeros,colores y tipos de pokemon

--- funcion.js
+++ funcion.js
@@ -7,17 +7,18 @@ for(let i= 1; i <= 151; i++){
 		.then(data => mostrarPokemon(data))
 }
 function mostrarPokemon(poke){
+	
 	const div = document.createElement("div");
 	div.classList.add("pokemon");
 	div.innerHTML = `
 	
 	<div class="img-container">
-	  <img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="Bulbasaur">
+	  <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
 	</div>
 	<div class="info">
-	  <span class="number">001</span>
+	  <span class="number">#${poke.id}</span>
 	  <h3 class="name">${poke.name}</h3>
-	  <small class="type">Type: <span>grass</span></small>
+	  <small class="type">Type: <span></span></small>
 	</div>`;
 
   pokecontainer.append(div);
